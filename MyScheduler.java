9f1bba3588c965bf7cfffa589c134e12de0bdc2a
Adrian Lopez <94017797+AdrianLopezP@users.noreply.github.com>
2024-03-22 14:19:30 -0400

    Added files via upload

---
+++ MyScheduler.java
@@ -0,0 +1,29 @@
+/**
+ * Models a thread-safe Blocking Queue.
+ *
+ * @author Alex Richardson
+ * @author Adrian Lopez
+ * @author Devin Rollins
+ **/
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class MyScheduler {
+
+    public MyScheduler(int numJobs, String property) {
+
+    
+    }
+    public LinkedBlockingQueue<Job> getOutgoingQueue() {
+        return null;
+    }
+    public LinkedBlockingQueue<Job> getIncomingQueue() {
+        return null;
+    }
+    public void run() {
+    
+    }
+}
+
+
