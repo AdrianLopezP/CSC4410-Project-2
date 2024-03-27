a44c928649d7a96a23abbb0499ecbc4960f33fec
DevRollins <drollins1108@gmail.com>
2024-03-27 13:51:21 -0400

    sum progress

--- MyScheduler.java
+++ MyScheduler.java
@@ -10,16 +10,37 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class MyScheduler {
-
+    
+    
     public MyScheduler(int numJobs, String property) {
 
-    
+        // Properties to check for: avg wait, max wait, combined(Max wait plus 2x avg wait), and deadlines(number of jobs met deadline)
+
+        // To optimize average wait, use 
+        // To optimize for deadlines, use EDF(Earliest Deadline First)
+        // To optimize for Job Length, use SJF(Shortest Job First)
+        LinkedBlockingQueue<Job> inQueue = new LinkedBlockingQueue<Job>();
+        LinkedBlockingQueue<Job> outQueue = new LinkedBlockingQueue<Job>();
+       
+       
     }
+
+
+
+
+    // Constructors
     public LinkedBlockingQueue<Job> getOutgoingQueue() {
-        return null;
+        // Jobs being sent to the CPU
+        //This is the queue where your code will put each Job that it has selected 
+        //to run on the CPU in the order it has chosen
+        return MyScheduler.;
+        // 
+
     }
     public LinkedBlockingQueue<Job> getIncomingQueue() {
-        return null;
+        // Jobs being sent from tester, to the scheduler
+        return inQueue;
+
     }
     public void run() {
     
