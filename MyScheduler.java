d0870afa6ab339481fe4c278e1f88d2aebdce3d6
DevRollins <drollins1108@gmail.com>
2024-04-10 16:57:12 -0400

    Actually started it this time

--- MyScheduler.java
+++ MyScheduler.java
@@ -16,21 +16,11 @@ public class MyScheduler {
     int scalingFactorOut = 0;
     int scalingFactorIn = 0;
     
-    
+    //MAIN CONSTRUCOR
     public MyScheduler(int numJobs, String property) {
-        // who knows what dis do
-        this.inQueue = new LinkedBlockingQueue<>(numJobs*scalingFactorIn);
-        // yo no se what dis do これをする分かりません
-        this.outQueue = new LinkedBlockingQueue<>(numJobs*scalingFactorOut);
+        this.inQueue = new LinkedBlockingQueue<Job>(numJobs*scalingFactorIn);
 
-        // Properties to check for: avg wait, max wait, combined(Max wait plus 2x avg wait), and deadlines(number of jobs met deadline)
-
-        // To optimize average wait, use 
-       
-        // To optimize for deadlines, use EDF(Earliest Deadline First)
-        
-        // To optimize for Job Length, use SJF(Shortest Job First)
-
+        this.outQueue = new LinkedBlockingQueue<Job>(numJobs*scalingFactorOut);
     }
 
     // Constructors
@@ -38,16 +28,51 @@ public class MyScheduler {
         // Jobs being sent to the CPU
         //This is the queue where your code will put each Job that it has selected 
         //to run on the CPU in the order it has chosen
-        return MyScheduler; 
-        // 
-
+        return outQueue; 
     }
+
     public LinkedBlockingQueue<Job> getIncomingQueue() {
         // Jobs being sent from tester, to the scheduler
         return inQueue;
+    }
+
+    //MAX WAIT - FCFS(First Come First Serve) - Blocking Que
+    public void maxWait(LinkedBlockingQueue<Job> in, LinkedBlockingQueue<Job> out){
+        BlockingQueue<Job> newBQueue = new LinkedBlockingQueue<Job>();
+        // While there are jobs in the incoming queue add them to the new Blocking Queue
+        while (!in.isEmpty()){
+            Job steve = in.remove();
+            newBQueue.add(steve);
+        }
+        
+        //add to out
+        while (!newBQueue.isEmpty()){
+            Job jobs = newBQueue.remove();
+            out.add(jobs);
+        }
+        //send off to cpu
+    }
+    //AVG WAIT - SJF(Shortest Job First) - Priority queue
+    public void avgWait(LinkedBlockingQueue<Job> in, LinkedBlockingQueue<Job> out){
 
     }
-    public void run() {
+
+    //DEAEDLINE - EDF(Earliest Deadline First) - Priority Queue
+    public void deadline(LinkedBlockingQueue<Job> in, LinkedBlockingQueue<Job> out){
+
+    }
+
+    //COMBINED - Fancy(Max wait + 2(Average Wait) ?????
+     public void combined(LinkedBlockingQueue<Job> in, LinkedBlockingQueue<Job> out){
+
+    }
+
+
+
+    
+    public void run(LinkedBlockingQueue<Job> inQueue, LinkedBlockingQueue<Job> outQueue) {
+        maxWait(inQueue, outQueue);
+
     
     }
 }
